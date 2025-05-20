b3cb7514f722a0a9b38fcd0232a88268fc032156
vlandero <vlad.andrei.rosu@gmail.com>
2025-05-20 14:38:35 +0300

    add small animations and clickable tooltips

--- presentation-app/src/components/ButtonWithTooltip.tsx
+++ presentation-app/src/components/ButtonWithTooltip.tsx
@@ -1,4 +1,5 @@
 import * as Tooltip from "@radix-ui/react-tooltip";
+import { useState } from "react";
 import { Button } from "./Button";
 import { FaInfo } from "react-icons/fa";
 
@@ -12,30 +13,36 @@ export const ButtonWithTooltip = ({
   tooltip: string;
   className?: string;
   onClick?: () => void;
-}) => (
-  <div className="flex items-center gap-2">
-    <Button className={className} onClick={onClick}>
-      {label}
-    </Button>
+}) => {
+  const [open, setOpen] = useState(false);
 
-    <Tooltip.Provider>
-      <Tooltip.Root>
-        <Tooltip.Trigger asChild>
-          <span className="cursor-pointer">
-            <FaInfo className="text-[#37a398]" />
-          </span>
-        </Tooltip.Trigger>
-        <Tooltip.Portal>
-          <Tooltip.Content
-            side="right"
-            sideOffset={5}
-            className="bg-black text-light text-s px-3 py-2 rounded-md shadow-lg z-50 max-w-[200px]"
-          >
-            {tooltip}
-            <Tooltip.Arrow className="fill-black" />
-          </Tooltip.Content>
-        </Tooltip.Portal>
-      </Tooltip.Root>
-    </Tooltip.Provider>
-  </div>
-);
+  const toggleTooltip = () => setOpen((prev) => !prev);
+
+  return (
+    <div className="flex items-center gap-2">
+      <Button className={className} onClick={onClick}>
+        {label}
+      </Button>
+
+      <Tooltip.Provider>
+        <Tooltip.Root open={open} onOpenChange={setOpen}>
+          <Tooltip.Trigger asChild>
+            <span onClick={toggleTooltip} className="cursor-pointer">
+              <FaInfo className="text-[#37a398]" />
+            </span>
+          </Tooltip.Trigger>
+          <Tooltip.Portal>
+            <Tooltip.Content
+              side="right"
+              sideOffset={5}
+              className="bg-black text-light text-s px-3 py-2 rounded-md shadow-lg z-50 max-w-[200px]"
+            >
+              {tooltip}
+              <Tooltip.Arrow className="fill-black" />
+            </Tooltip.Content>
+          </Tooltip.Portal>
+        </Tooltip.Root>
+      </Tooltip.Provider>
+    </div>
+  );
+};
--- presentation-app/src/pages/index.tsx
+++ presentation-app/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Check } from "lucide-react";
 import { pricingPlans } from "@/constants/pricing";
 import Logo from "@/components/Logo";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const [modalSignupOpen, setModalSignupOpen] = useState(false);
@@ -19,18 +20,38 @@ export default function Home() {
     };
   }, [modalSignupOpen]);
 
+  const fadeInFromLeft = (delay: number) => ({
+    hidden: { opacity: 0, x: -50 },
+    visible: (i: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: i * 0.2,
+        duration: delay,
+        ease: "easeOut",
+      },
+    }),
+  });
+
   return (
     <div className="scroll-smooth">
       <Logo />
 
       <section className="md:min-h-[50vh] lg:h-[50vh] bg-primary-gradient flex flex-row justify-center">
         <div className="h-full w-[90%] max-w-[1700px] text-white grid grid-cols-1 md:grid-cols-3 text-center px-4 gap-10 py-16">
-          <div className="md:col-span-2 flex items-center justify-center rounded-2xl glass-panel flex-col md:flex-row p-6 shadow-md">
+          <motion.div
+            custom={0}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInFromLeft(0.8)}
+            className="md:col-span-2 flex items-center justify-center rounded-2xl glass-panel flex-col md:flex-row p-6 shadow-md gap-4 md:gap-8"
+          >
             <div className="md:w-1/2 text-left md:pr-6">
               <h1 className="text-3xl md:text-4xl font-bold mb-4">
                 All your teams, one place.
               </h1>
-              <p className="text-lg max-w-md mb-6">
+              <p className="text-lg max-w-md">
                 Manage your entire organization from a single dashboard—no
                 chaos, no confusion.
               </p>
@@ -38,11 +59,18 @@ export default function Home() {
             <img
               src="/registry.png"
               width={400}
-              className="md:w-1/2 mt-6 md:mt-0 transition-transform duration-500 hover:scale-180 hover:rotate-10 hover:drop-shadow-xl rounded-2xl"
+              className="md:w-1/2 transition-transform duration-500 hover:scale-180 hover:rotate-10 hover:drop-shadow-xl rounded-2xl"
             />
-          </div>
+          </motion.div>
 
-          <div className="md:col-span-1 flex items-center justify-center glass-panel rounded-2xl flex-col p-6 shadow-md">
+          <motion.div
+            custom={0}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInFromLeft(1)}
+            className="md:col-span-1 flex items-center justify-center glass-panel rounded-2xl flex-col p-6 shadow-md"
+          >
             <h2 className="text-3xl md:text-4xl font-bold mb-4">
               One platform. Endless control.
             </h2>
@@ -53,17 +81,24 @@ export default function Home() {
             <Button onClick={() => setModalSignupOpen(true)}>
               Get Started for free!
             </Button>
-          </div>
+          </motion.div>
         </div>
       </section>
 
       <section className="md:min-h-[50vh] lg:h-[50vh] secondary-gradient flex flex-row justify-center">
-        <div className="w-[90%] max-w-[2500px] text-white grid grid-cols-1 md:grid-cols-5 text-center px-4 py-16 gap-8">
-          <div className="md:col-span-3 flex flex-col-reverse glass-panel items-center justify-center p-6 rounded-2xl shadow-md bg-white/5 gap-10 md:flex-row">
+        <div className="w-[90%] max-w-[2500px] text-white grid grid-cols-1 md:grid-cols-5 text-center px-4 py-16 gap-3 md:gap-8">
+          <motion.div
+            custom={0}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInFromLeft(1.2)}
+            className="md:col-span-3 flex flex-col-reverse glass-panel items-center justify-center p-6 rounded-2xl shadow-md bg-white/5 gap-3 md:gap-10 md:flex-row"
+          >
             <img
               src="/analysis.png"
               width={400}
-              className="md:w-1/2 mt-6 md:mt-0 transition-transform duration-500 hover:scale-200 hover:-rotate-12 hover:drop-shadow-xl  rounded-2xl"
+              className="md:w-1/2 transition-transform duration-500 hover:scale-200 hover:-rotate-12 hover:drop-shadow-xl rounded-2xl"
             />
             <div>
               <h2 className="text-2xl font-bold mb-2">Your Own Analyst</h2>
@@ -73,15 +108,29 @@ export default function Home() {
                 data-driven insights to improve faster.
               </p>
             </div>
-          </div>
-          <div className="md:col-span-1 flex flex-col glass-panel items-center justify-center p-6 rounded-2xl shadow-md bg-white/5">
+          </motion.div>
+          <motion.div
+            custom={0}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInFromLeft(1.4)}
+            className="md:col-span-1 flex flex-col glass-panel items-center justify-center p-6 rounded-2xl shadow-md bg-white/5"
+          >
             <h2 className="text-2xl font-bold mb-2">Plug & Play Growth</h2>
             <p className="text-lg font-medium text-zinc-100">
               Expand your team structure or game coverage effortlessly as you
               scale.
             </p>
-          </div>
-          <div className="md:col-span-1 flex flex-col glass-panel items-center justify-center p-6 rounded-2xl shadow-md bg-white/5">
+          </motion.div>
+          <motion.div
+            custom={0}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={fadeInFromLeft(1.6)}
+            className="md:col-span-1 flex flex-col glass-panel items-center justify-center p-6 rounded-2xl shadow-md bg-white/5"
+          >
             <h2 className="text-2xl font-bold mb-2">
               Effortless Collaboration
             </h2>
@@ -89,7 +138,7 @@ export default function Home() {
               Bring coaches, players, and staff together in a seamless and
               transparent workspace.
             </p>
-          </div>
+          </motion.div>
         </div>
       </section>
 
